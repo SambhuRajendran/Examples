8ffffd37c32e54913306bee5c4550218f2873d3d
sambhu619 <rsambhu619@gmail.com>
2023-08-07 17:34:10 -0400

    First commit

---
+++ printnum.py
@@ -0,0 +1,14 @@
+n = 10
+for i in range(1,n+1):
+    for j in range(1,i):
+      print(j,end=' ')
+    print()
+
+    if i == 6:
+       
+       for j in range(1,n-i):
+          print(j, end=" ")
+    print()
+
+             
+ 
