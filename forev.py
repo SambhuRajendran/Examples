1eb3d3c3487b59697c6f3825c75cfa901182a972
sambhu619 <rsambhu619@gmail.com>
2023-08-07 17:34:29 -0400

    initial commit

---
+++ forev.py
@@ -0,0 +1,7 @@
+n = 6
+for i in range (1,n):
+    
+    for j in range(n-i):
+        print("*")
+        #print("same", end=" ")
+    print()
---
+++ helloworldfunction.py
@@ -0,0 +1,8 @@
+def main():
+    name = input("What's your name: ")
+    hello()
+
+def hello(to="World"):
+    print("hello, ", to)
+
+main()
