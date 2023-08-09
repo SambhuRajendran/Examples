373cbab190af9dc47f3a6da899296d488b7f0314
sambhu619 <rsambhu619@gmail.com>
2023-08-09 12:28:53 -0400

    listComprehension.py

--- forloop.py
+++ forloop.py
@@ -3,24 +3,3 @@ for i in range(1, n+1):
         for j in range(0,i):
             print('*', end=" ")
         print()
-
-
-i=1
-j=0
-
-* 
-
-i=2
-j= 0,1
-
-* * 
-
-i=3
-j= 0,1 2 * * *
-
-i=4
-j= ) 1 2 3 * * * *
-
-i=5 
-j= 0 1 2 3 4 * * * * * 
-            
