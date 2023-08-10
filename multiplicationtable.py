e9108735915127c0269446682f751799fb4b396c
sambhu619 <rsambhu619@gmail.com>
2023-08-10 18:27:15 -0400

    modified:   multiplicationtable.py
        Now accept multiple user inputs

--- multiplicationtable.py
+++ multiplicationtable.py
@@ -1,5 +1,16 @@
-number = int(input('Enter the number: '))
-until = int(input('Until what number: '))
+# MULTIPLICATION TABLE
+#This program takes multiple number from user to multiply until the range of number they provide
+number = (input('Enter the numbers seperated by comma: ')).split(',')
+until = (int(input('Until what number: ')))
+numbers =[] #Empty list to append integers
+for i in number:
+     numbersint = int(i)#Converting numbers to integer
+     numbers.append(numbersint)
+print(numbers)
+print(type(numbers))
 
-for i in range(1,until+1):
-    print(f'{number} multiplied by {i} is: ', number*i)
+for i in numbers:
+    for j in range(1, until+1):
+        print(f'{i} multiplied by {j} is: ',i*j)
+
+
