ad25ed6f247635aaa3b86564bd3cbe9360938a0f
sambhu619 <rsambhu619@gmail.com>
2023-08-10 12:40:50 -0400

    modified:   listComprehension.py

--- listComprehension.py
+++ listComprehension.py
@@ -1,5 +1,4 @@
 #get a list of words
-import string
 words = input("Enter words you want (comma=seperated): ").split(',')
 for i in range(len(words)):
     newword = words[i].strip() #remove any leading and trailing spaces
