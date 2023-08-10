aa0025036039e291442f42b626a5f830134f1035
sambhu619 <rsambhu619@gmail.com>
2023-08-10 18:34:53 -0400

    modified:   multiplicationtable.py

--- multiplicationtable.py
+++ multiplicationtable.py
@@ -1,13 +1,11 @@
 # MULTIPLICATION TABLE
 #This program takes multiple number from user to multiply until the range of number they provide
-number = (input('Enter the numbers seperated by comma: ')).split(',')
+number = (input('Enter the numbers separated by comma: ')).split(',')
 until = (int(input('Until what number: ')))
 numbers =[] #Empty list to append integers
 for i in number:
-     numbersint = int(i)#Converting numbers to integer
-     numbers.append(numbersint)
-print(numbers)
-print(type(numbers))
+     #Converting numbers to integer
+     numbers.append(int(i))
 
 for i in numbers:
     for j in range(1, until+1):
