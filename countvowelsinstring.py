dc71f2533f846b0be48e448aaadb8ddbfb1e5c9e
sambhu619 <rsambhu619@gmail.com>
2023-08-17 23:13:44 -0400

    modified:   countvowelsinstring.py

--- countvowelsinstring.py
+++ countvowelsinstring.py
@@ -1,16 +1,13 @@
 word = input('Enter a word to calculate number of vowels: ')
 count = 0
 i=0
+vowels = ['a', 'e', 'i', 'o', 'u']
+
 
 for i in range(len(word)):
-    if ((word[i] == 'a')
-        or (word[i] == 'e')
-        or (word[i] == 'i')
-        or (word[i] == 'o')
-        or (word[i] == 'u')
-        ):
+    if word[i] in vowels:
+        print(word[i], ' is a vowel')
         count = count + 1
-
 print(count)
     
 
