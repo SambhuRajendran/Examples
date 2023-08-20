29485de9290fb4ffde126eaa4397fe722e6dd8c1
sambhu619 <rsambhu619@gmail.com>
2023-08-20 19:43:45 -0400

    modified:   numberlessthan5.py

--- numberlessthan5.py
+++ numberlessthan5.py
@@ -1,16 +1,11 @@
 numbers = (input("Enter the numbers you want to check for 5: ")).split(',')
-print(type(numbers))
-print(numbers)
 for i in range(0, len(numbers)):
     numbers[i]=int(numbers[i])
 
-print((numbers))
-print(type(numbers))
 count=0
 for i in numbers:
     if i < 5:
-        
-        print(f'{i}, is less than 5')
+                print(f'{i}, is less than 5')
     else:
         count = count+1
         continue
