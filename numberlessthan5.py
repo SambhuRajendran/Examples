24d2def9d5be09bb8452a92744d7240f1b65886d
sambhu619 <rsambhu619@gmail.com>
2023-08-20 19:43:03 -0400

    modified:   numberlessthan5.py

--- numberlessthan5.py
+++ numberlessthan5.py
@@ -6,9 +6,13 @@ for i in range(0, len(numbers)):
 
 print((numbers))
 print(type(numbers))
-
+count=0
 for i in numbers:
     if i < 5:
+        
         print(f'{i}, is less than 5')
     else:
+        count = count+1
         continue
+
+print(f'There are {count} inputs that are greater than 5')
