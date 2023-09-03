e790504b457687875d29b981a06c72fe4de03c1b
sambhu619 <rsambhu619@gmail.com>
2023-09-03 11:16:47 -0400

    modified:   calculatorfn.py

--- calculatorfn.py
+++ calculatorfn.py
@@ -1,12 +1,14 @@
 def addition():
-    numbers = int(input('How many numbers you want to Add: '))
-    listofnumbers= []
-    for _ in range(1, numbers+1):
-        value = int(input('Enter the value: '))
-        listofnumbers.append(value)
-    print(f'Sum of the given values = {sum(listofnumbers)}. Thanks for using the Addition function.')
+    try: 
+      numbers = int(input('How many numbers you want to Add: '))
+      listofnumbers= []
+      for _ in range(1, numbers+1):
+          value = int(input('Enter the value: '))
+          listofnumbers.append(value)
+      print(f'Sum of the given values = {sum(listofnumbers)}. Thanks for using the Addition function.')
+    except ValueError:
+        print('Please enter a valid number. Try again.')
 
-#addition()    
 
 def multiplication():
     try: 
@@ -18,8 +20,18 @@ def multiplication():
       initial = 1
       for n in listofnumbers:
           initial = initial * n
-      print(initial)
+      print(f'Multiplication of the given values = {initial}. Thanks for using the Multiplication function.')
     except ValueError:
-        print('Enter a valid number. Try Again.')
+        print('Please enter a valid number. Try Again.')
         
-multiplication()
+print('Hello, Welcome to the calculator program')
+print('Press "1" for Addition, "2" for Multiplicaton')
+try:
+  userinput = int(input('What action you want to perform: '))
+  if userinput == 1:
+     addition()
+  elif userinput == 2:
+     multiplication()
+except ValueError:
+   print("Please give a valid input. Try Again. Thanks!")
+  
